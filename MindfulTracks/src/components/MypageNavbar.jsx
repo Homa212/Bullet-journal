4606127dbefe36d024cb2ca7e4d6b039fafa2b0c
MelanieKaller <melaniekaller98@gmail.com>
2024-03-22 11:16:12 +0100

    MyPageNavbar is done

--- MindfulTracks/src/components/MypageNavbar.jsx
+++ MindfulTracks/src/components/MypageNavbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function MypageNavbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return(
         
@@ -31,18 +37,18 @@ function MypageNavbar() {
                 </div>
 
                 {/* hamburger menu */}
-                <div className="sm:hidden w-screen flex justify-between px-10">
-                    <button className="border border-emerald-900 rounded-lg p-2 my-5 text-5xl font-bold">Mindful Trackers</button>
-                    <button className='group sm:hidden'>
-                        <svg class="w-12 h-12 text-gray-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                <div className="sm:hidden w-screen flex justify-between px-5">
+                    <button className="my-5 text-4xl font-bold">Mindful Trackers</button>
+                    <button className='group sm:hidden' onClick={toggleMenu}>
+                        <svg class="w-12 relative h-12 text-gray-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                         fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
                     
                         {/* menu */}
-                        <ul className='bg-amber-50 mt-18 w-fit z-10 text-4xl p-5 absolute -top-full group-focus:top-0 right-0 duration-150 flex flex-col space-y-3 justify-end'>
-                            <button className='px-10 py-8 relative ml-auto'>
+                        <ul className={`bg-amber-50 mt-20 w-fit z-10 text-4xl p-5 pt-0 absolute -top-full group-focus:top-0 right-0 duration-150 flex flex-col space-y-3 justify-end ${isMenuOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10'}`}>
+                            {/* <button className='px-10 py-8 relative ml-auto'>
                                 <div className='w-6 h-1 rotate-45 absolute bg-black'></div>
                                 <div className='w-6 h-1 -rotate-45 absolute bg-black'></div>
-                            </button>
+                            </button> */}
                             <div className="flex flex-col justify-between xl:w-60 sm:w-40 items-start gap-5">
                                 <button className="relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:-bottom-3 before:-left-0 before:w-0 before:h-0.5 before:rounded-full before:opacity-0 before:transistion-all before:duration-500 before:bg-gradient-to-r before:from-emerald-700 before:via-emerald-600 before:to-green-50 hover:before:w-full hover:before:opacity-100 px-1">
                                     <Link to="/MyProfilePage">My profile</Link>
