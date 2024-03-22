fe3f220de80cccbccf418aca7a3961b31d957c7d
Homa Khan <homa97@hotmail.se>
2024-03-22 11:42:52 +0100

    password reset backend

--- Backend/auth_endpoints.py
+++ Backend/auth_endpoints.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, HTTPException, Depends, status, APIRouter
 from app.db_setup import init_db, get_db
 from contextlib import asynccontextmanager
-from fastapi import Request
+from fastapi import Request, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session, joinedload, selectinload, load_only
 from sqlalchemy import select, update, delete, insert
 from app.models import User, WeeklyPlan, Habit, Todo, SleepTracker, WorkoutTracker, JournalYourDay, MoodTracker
-from app.schemas.schemas import Token, UserOutSchema, UserRegisterSchema, WeeklyPlanSchema, HabitSchema, TodoSchema, SleepTrackerSchema, WorkoutTrackerSchema, JournalYourDaySchema, MoodTrackerSchema 
+from app.schemas.schemas import NewPasswordSchema, Token, UserOutSchema, UserRegisterSchema, WeeklyPlanSchema, HabitSchema, TodoSchema, SleepTrackerSchema, WorkoutTrackerSchema, JournalYourDaySchema, MoodTrackerSchema 
 from sqlalchemy.exc import IntegrityError, NoResultFound
 from app.security import hash_password, verify_password, create_access_token, get_current_user
 from fastapi.security import OAuth2PasswordRequestForm
@@ -14,6 +14,8 @@ from typing import Annotated
 from datetime import timedelta, datetime
 from dotenv import load_dotenv 
 import os
+from app.email import generate_password_reset_token, send_password_reset_email, get_user_by_email, verify_password_reset_token
+
 
 load_dotenv(override=True)
 
@@ -64,4 +66,42 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Sessio
 @router.get("/me")
 def read_users_me(current_user: Annotated[User, Depends(get_current_user)]) -> UserOutSchema:
     return current_user
+
+@router.post("/password-recovery/{email}", status_code=status.HTTP_200_OK)
+def recover_password(email: str, background_tasks: BackgroundTasks, db : Session = Depends(get_db)):
+    """
+    Password Recovery
+    Sends a password reset email
+    """
+    user = get_user_by_email(session=db, email=email)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this email does not exist in the system.",
+        )
+    password_reset_token = generate_password_reset_token(email=email)
+    background_tasks.add_task(send_password_reset_email, email, password_reset_token)
+    # send_password_reset_email(email=email, token=password_reset_token)
+    return {"message": "Mail sent"}
+
+
+@router.post("/reset-password/", status_code=status.HTTP_200_OK)
+def reset_password(body: NewPasswordSchema, db: Session = Depends(get_db)):
+    """
+    Reset password using a token a user should have received through a PW reset email
+    """
+    email = verify_password_reset_token(token=body.token)
+    if not email:
+        raise HTTPException(status_code=400, detail="Invalid token")
+    user = get_user_by_email(session=db, email=email)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this email does not exist in the system.",
+        )
+    hashed_password = hash_password(password=body.new_password)
+    user.password = hashed_password
+    db.add(user)
+    db.commit()
+    return {"message": "Password updated"}
     
